7b63d95ebdbf0c80ee1ffc2d7f5400835e92b397
Suneeth <suneeth.since2005@gmail.com>
2023-04-30 22:50:01 +0530

    Create asymptonic_notations.js

---
+++ 1. asymptonic_notations/asymptonic_notations.js
@@ -0,0 +1,8 @@
+/**
+* Asymptonic Notations
+* - these are the mathmatical representations for time and space complexity
+* 
+* 1. Big O Notation - worst case complexity
+* 2. Omega Notation - best case complexity
+* 3. Theta Notation - average case complextity
+*/
